c102dfad1858081e1dd5484454cef23212cb20a9
Roberto Nicoletti <roberto.urias@gmail.com>
2020-03-26 20:47:33 -0300

    Criando tabelas ong e incidents com knex

---
+++ aulas/backend/src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const routes = require("./routes");
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP
+ *
+ * GET: Busca/Listar informações no back-end
+ * POST: Criar informações no back-end
+ * PUT: Alterar informações no back-end
+ * DELETE: Deletar informações no back-end
+ */
+
+/**
+ * Tipos de parâmetros
+ *
+ * Query: request.query: Params: Parâmetros nomeados eviandos na rota após "?" (Filtros, paginação)
+ * Route Params: request.params: Parâmetros utilizados para identifcar recursos
+ * Request Body: request.body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreeSQL, Oracle, SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
