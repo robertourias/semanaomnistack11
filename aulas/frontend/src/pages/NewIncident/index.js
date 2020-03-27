e0fd329fb01a86bb7399604b5c8d9f791b27a464
Roberto Nicoletti <roberto.urias@gmail.com>
2020-03-27 17:54:31 -0300

    Integração do novo caso cadastrado

--- aulas/frontend/src/pages/NewIncident/index.js
+++ aulas/frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
+import api from "../../services/api";
 import "./styles.css";
 import { FiArrowLeft } from "react-icons/fi";
 import logoSvg from "../../assets/logo.svg";
 
 export default function NewIncident() {
+  const ongId = localStorage.getItem("ongId");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value
+    };
+
+    try {
+      await api.post("incidents", data, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+
+      history.push("/profile");
+    } catch (err) {
+      alert("Não foi possível cadastrar, tente novamente.");
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -24,10 +53,22 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Decrição" />
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Título do caso"
+          />
+          <textarea
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            placeholder="Decrição"
+          />
+          <input
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            placeholder="Valor em reais"
+          />
 
           <button className="button" type="submit">
             Cadastrar
