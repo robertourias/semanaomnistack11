d2bec9b6ff4f0fa27d27412d5a551cb505b6a78f
Roberto Nicoletti <roberto.urias@gmail.com>
2020-03-27 17:05:50 -0300

    Integração da página do Login criando localStorage

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiLogIn } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
+import api from "../../services/api";
 import "./styles.css";
 import logoSvg from "../../assets/logo.svg";
 import heroesPng from "../../assets/heroes.png";
 
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+      history.push("/profile");
+    } catch (err) {
+      alert("Erro no login, tente novamente.");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoSvg} alt="be the Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Seu ID" />
+          <input
+            value={id}
+            onChange={e => setId(e.target.value)}
+            placeholder="Seu ID"
+          />
           <button className="button" type="submit">
             Entrar
           </button>
