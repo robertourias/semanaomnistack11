1c14ea016034e49a6c23b40ac2efe585d3dde972
Roberto Nicoletti <roberto.urias@gmail.com>
2020-03-27 22:40:03 -0300

    Criando sistemas de rotas

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View>
-      <Text>Semana Omnistack 4</Text>
-    </View>
-  );
+  return <Routes />;
 }
