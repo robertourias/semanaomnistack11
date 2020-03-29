ba936ed66fd8e1e23828cbb01841508f8215e3b8
Roberto Nicoletti <roberto.urias@gmail.com>
2020-03-29 17:19:04 -0300

    Criando paginação

--- aulas/mobile/src/pages/Incidents/index.js
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -10,15 +10,27 @@ import logoImg from "../../assets/logo.png";
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState();
   const navigation = useNavigation();
 
   async function loadIncidents() {
+    if (loading || (total > 0 && incidents.length === total)) {
+      return;
+    }
+
+    setLoading(true);
+
     try {
-      const response = await api.get("incidents");
+      const response = await api.get("incidents", {
+        params: { page }
+      });
       console.log("DATA:", response.data);
 
-      setIncidents(response.data);
+      setIncidents([...incidents, ...response.data]);
       setTotal(response.headers["x-total-count"]);
+      setPage(page + 1);
+      setLoading(false);
     } catch (err) {
       console.log("Error: ", err);
     }
@@ -51,6 +63,8 @@ export default function Incidents() {
         data={incidents}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
